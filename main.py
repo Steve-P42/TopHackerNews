8897803b26c039f9c9cea29636ec058b2d9babf2
Steve-P42 <78639312+Steve-P42@users.noreply.github.com>
2021-03-13 18:11:53 +0100

    setup class, init method and method to get page content from HN

--- main.py
+++ main.py
@@ -6,15 +6,27 @@
 # Status:           in development
 # ----------------------------------------------------------------------------
 # %%
+import requests
+from bs4 import BeautifulSoup
+
+# %%
+class TopHackerNews:
+    def __init__(self):
+        self.source = ['https://news.ycombinator.com/news', 'https://news.ycombinator.com/news?p=2']
+        self.page_contents = self.get_page_contents()
 
 
-
+    def get_page_contents(self):
+        res1 = requests.get(self.source[0])
+        res2 = requests.get(self.source[1])
+        soup = BeautifulSoup(res1.text + res2.text, "html.parser")
+        return soup
 
 # %%
 
-# %%
+x = TopHackerNews()
 
-# %%
+print(x.page_contents)
 
 # %%
 
